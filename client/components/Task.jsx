511c817b7e7d2687bd04148cc1627384c81b1966
Kais <kais.lutfi1@gmail.com>
2022-07-05 15:14:08 +1200

    reminder class

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 
 export const Task = ( {task, onDelete, onToggle}) => {
   return (
-    <div className='task' onDoubleClick={() =>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() =>
       onToggle(task.id)}>
       <h3>{task.task_name} <FaTimes onClick={() =>
         onDelete(task.id)}/></h3>
